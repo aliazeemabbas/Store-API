4d4c2abd389e0a98cefc83d7a2297ff7823d0d2c
aliazeemabbas <aliazeem480@gmail.com>
2023-09-08 23:04:17 +0500

    added router and controllers

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
-const express = require('express')
+require('dotenv').config()
 
-const app = express()
+//async errors
 
+const express = require('express');
+
+const app = express();
+
+const connectDB = require('./db/connect')
+const productsRouter = require('./routes/products')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+app.use(express.json())
+
+//routes
 app.get('/', (req, res) => {
-    res.send('Home Page')
+    res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>')
 })
 
-app.listen(5000, () => {
-    console.log('Server is listening on port 5000...');
-})
+app.use('/api/v1/products', productsRouter)
+
+//products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 5000
+
+const start = async () => {
+    try {
+        //connectDB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
