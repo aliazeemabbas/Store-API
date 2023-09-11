316e7ebefb71f41eff48795bcdf50ffee739addb
aliazeemabbas <aliazeem480@gmail.com>
2023-09-11 07:22:25 +0500

    regex search in static API

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,26 @@
 const Product = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
+    const search = 'ab'
     const products = await Product.find({
-        name: 'vase table'
+        name: { $regex: search, $options: 'i' }
     })
     res.status(200).json({ products })
 }
 
 const getAllProducts = async (req, res) => {
-    const products = await Product.find(req.query);
+    const { featured, company, name } = req.query
+    const queryObject = {}
+    if(featured){
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if(company){
+        queryObject.company = company
+    }
+    if(name){
+        queryObject.name = name
+    }
+    const products = await Product.find(queryObject);
     res.status(200).json({ products })
 }
 
